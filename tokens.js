6bc0c1e1e237bad05baf95f2eb3304b24cc95113
cacaimut <baiqlalucacaputri@gmail.com>
2026-07-25 22:47:32 +0800

    Update token list

--- tokens.js
+++ tokens.js
@@ -2,6 +2,6 @@
   "tokens": [
     "7584438387:AAGtcsZoAYm0F0QKgjrrGE5GrlQrpdaO7vQ",
     "8786834932:AAEOx8mACKjjiVcqLujHz0VI4RyghTTcxvA",
-    "8955723917:AAHblw1jVAcXgYlDIDumW1cSiQBcIjPfDh8"
+    "1"
   ]
 }
