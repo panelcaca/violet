e4d2e282b0dcd7d9e3d8a6d13f22534273d0d645
cacaimut <baiqlalucacaputri@gmail.com>
2026-07-26 14:44:46 +0800

    Update token list

--- tokens.js
+++ tokens.js
@@ -7,6 +7,7 @@
     "8669574389:AAHMZQ92dEPxnr6KwgbARIRmHF_1Cv1zx7Y",
     "token kamu",
     "8855096524:AAH9ygEIE1WcowvZYqKEURghw88vbbjIoaE",
-    "8416153347:AAGGxdDq7as0mbzn8Q-iJeXvixiHPW8iA0I"
+    "8416153347:AAGGxdDq7as0mbzn8Q-iJeXvixiHPW8iA0I",
+    "8888476982:AAGICEEpGAbwVmfOA-vM0VJpsKyHSekRfDc"
   ]
 }
