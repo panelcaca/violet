15954409b45b80524d33401845f39dfcd3bb847c
cacaimut <baiqlalucacaputri@gmail.com>
2026-07-26 19:16:59 +0800

    Update token list

--- tokens.js
+++ tokens.js
@@ -11,6 +11,7 @@
     "8888476982:AAGICEEpGAbwVmfOA-vM0VJpsKyHSekRfDc",
     "8758055239:AAE7CJ98v_alUSy5H8iMj3ZYKPAUhBzQjPU",
     "8587268852:AAH1tXoDz0IcPRxBk_edVUi7AxZC2AqBch0",
-    "dulu"
+    "dulu",
+    "8811649747:AAEElqAa-CJnHlU9YBdfGLcu6FyR-uSuC4I"
   ]
 }
