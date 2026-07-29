56478e9d68a707f3ac90bdb4012e5d27e2a9a1ca
cacaimut <baiqlalucacaputri@gmail.com>
2026-07-29 17:33:20 +0800

    🔄 Bot: Removed a bot token from object database

--- tokens.js
+++ tokens.js
@@ -2,7 +2,6 @@
   "tokens": [
     "7584438387:AAGtcsZoAYm0F0QKgjrrGE5GrlQrpdaO7vQ",
     "8883447090:AAHY9MEubPFqYNZBCBZPCUsJWJRIus5tARE",
-    "token kamu",
-    "8855096524:AAH9ygEIE1WcowvZYqKEURghw88vbbjIoaE"
+    "token kamu"
   ]
 }
